86b457f3f51fc02b78efe3236009b696c7a15112
carolinavitoria786 <142808135+carolinavitoria786@users.noreply.github.com>
2023-10-13 08:51:18 -0300

    commit

---
+++ pratica01/src/pratica01/pratica01_ex2.java
@@ -0,0 +1,25 @@
+package pratica01;
+
+import java.util.Scanner;
+
+public class pratica01_ex2 {
+
+	public static void main(String[] args) {
+		  Scanner input = new Scanner(System.in);
+		 
+		  int anonascimento, anoatual, idade;
+		  
+		  System.out.println("Informe o ano de seu nascimento:");
+		  anonascimento = input.nextInt();
+		  
+		  System.out.println("Informe o ano em que você está:");
+		  anoatual = input.nextInt();
+		  
+		  idade = anoatual - anonascimento;
+		  
+		  System.out.println("Sua idade é:" + idade);
+		  
+		  input.close();
+		}
+
+	}

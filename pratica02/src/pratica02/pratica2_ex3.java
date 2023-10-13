7c35db3b0a315bd729e09b72dd6a8ddf61f85c7b
carolinavitoria786 <142808135+carolinavitoria786@users.noreply.github.com>
2023-10-13 08:45:00 -0300

    commit

--- pratica02/src/pratica02/pratica2_ex3.java
+++ pratica02/src/pratica02/pratica2_ex3.java
@@ -1,10 +1,34 @@
 package pratica02;
 
+import java.util.Scanner;
+
 public class pratica2_ex3 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Digite a primeira nota do aluno (entre 0 e 10):");
+		double nota1 = scanner.nextDouble();
+
+		System.out.println("Digite a segunda nota do aluno (entre 0 e 10):");
+		double nota2 = scanner.nextDouble();
+
+		System.out.println("Digite a terceira nota do aluno (entre 0 e 10):");
+		double nota3 = scanner.nextDouble();
+
+		double media = (nota1 + nota2 + nota3) / 3.0;
+
+		System.out.println("Média do aluno: " + media);
+
+		if (media >= 0 && media < 3) {
+			System.out.println("REPROVADO");
+		} else if (media >= 3 && media < 7) {
+			System.out.println("EXAME");
+		} else if (media >= 7 && media <= 10) {
+			System.out.println("APROVADO");
+		} else {
+			System.out.println("Média inválida. Certifique-se de que todas as notas estejam no intervalo [0, 10].");
+		}
+
+		scanner.close();
 	}
-
 }

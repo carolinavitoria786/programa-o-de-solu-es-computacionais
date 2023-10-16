852d071389fa5aa3bf36e13738ab08822c9a74c2
carolinavitoria786 <142808135+carolinavitoria786@users.noreply.github.com>
2023-10-16 07:24:43 -0300

    commit

--- Pratica10_POO/src/pratica10/pratica10_ex2.java
+++ Pratica10_POO/src/pratica10/pratica10_ex2.java
@@ -1,10 +1,36 @@
 package pratica10;
 
-public class pratica10_ex2 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+private class Poo{
 
+*// privado para armarzena o nome das pessoas//*
+
+	    public void setNome(String novoNome) {
+	        this.nome = novoNome;
+	    }
+	    
+			public String getNome() {
+	        return this.nome;
+	    }
 	}
 
-}
+	public class Main {
+	    public static void main(String[] args) {
+	        
+	        Poo[] vetorPessoas = new Poo[5];
+	        
+	        vetorPessoas[0] = new Poo();
+	        vetorPessoas[0].setNome("João");
+
+	        vetorPessoas[1] = new Poo();
+	        vetorPessoas[1].setNome("Maria");
+
+	        *//nome das pessoas//* 
+	        for (int i = 0; i < vetorPessoas.length; i++) {
+	            if (vetorPessoas[i] != null) {
+	                System.out.println("Nome da pessoa " + i + ": " + vetorPessoas[i].getNome());
+	            }
+	        }
+	    }
+	}
--- Pratica11_POO/src/pratica11/pratica11_ex2.java
+++ Pratica11_POO/src/pratica11/pratica11_ex2.java
@@ -2,9 +2,57 @@ package pratica11;
 
 public class pratica11_ex2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public int multiplicacao(int a, int b) {
+        int resultado = 0;
+        if (b > 0) {
+            for (int i = 0; i < b; i++) {
+                resultado += a;
+            }
+        } else if (b < 0) {
+            for (int i = 0; i > b; i--) {
+                resultado -= a;
+            }
+        }
+        return resultado;
+    }
 
-	}
+    public int potencia(int a, int b) {
+        int resultado = 1;
+        if (b >= 0) {
+            for (int i = 0; i < b; i++) {
+                resultado = multiplicacao(resultado, a);
+            }
+        } else {
+     
+            throw new IllegalArgumentException("Expoente negativo não é suportado");
+        }
+        return resultado;
+    }
 
+    public int divisao(int a, int b) {
+        int quociente = 0;
+        while (a >= b) {
+            a = a - b;
+            quociente++;
+        }
+        return quociente;
+    }
+    public int resto(int a, int b) {
+        while (a >= b) {
+            a = a - b;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+    	
+    	pratica11_ex2 listaonzeexdois = new pratica11_ex2(); 
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Multiplicação: " + listaonzeexdois.multiplicacao(a, b));
+        System.out.println("Potência: " + listaonzeexdois.potencia(a, b));
+        System.out.println("Divisão (Quociente): " + listaonzeexdois.divisao(a, b));
+        System.out.println("Resto: " + listaonzeexdois.resto(a, b));
+    }
 }
